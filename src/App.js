1743185d7c394b26c88f2ecbd5fbb6bc3f6f8ce9
rhammer35 <rhammer35@icloud.com>
2021-03-19 13:16:56 -0400

    added states for character info and character id for expanding character info and created effect hook for character API call in app.js; also created theme file and details file to use

--- src/App.js
+++ src/App.js
@@ -1,17 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
+import Character from './components/Character';
+import Details from './components/Details';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [characters, setCharacters] = useState([]);
+  const [currentCharId, setCurrentCharId] = useState('1');
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people/')
+      .then(res => {
+        setCharacters(res.data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, [])
+
+  const openDetails = id => {
+    setCurrentCharId(id);
+  }
+
+  const closeDetails = () => {
+    setCurrentCharId(null);
+  }
+
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {
+        characters.map(char => {
+          return <Character key={char.id} info={char} action={openDetails} />
+        })
+      }
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,29 @@
 // Write your Character component here
+import React from 'react';
+import styled, {keyframes} from 'styled-components';
+
+const StyledCharacter = styled.section`
+  color: ${prop => prop.theme.primaryColor};
+  width: 60%;
+  display: flex;
+  justify-content: space-between;
+
+  &:hover {
+    color: ${prop => prop.theme.tertiaryColor};
+  }
+
+  button {
+    color: ${prop => prop.theme.secondaryColor};
+  }
+`
+
+export default function Character({ info, action }) {
+  return (
+    <StyledCharacter danger={info.name === 'Devin'}>
+      {info.name}
+      <button onClick={() => action(info.id)}>
+        See More
+      </button>
+    </StyledCharacter>
+  )
+}
